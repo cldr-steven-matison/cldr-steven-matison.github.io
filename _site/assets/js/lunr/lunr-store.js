be0bfb6f1bedd3b27afd68d543644cae19e8de25
Steven Matison <98845751+cldr-steven-matison@users.noreply.github.com>
2023-05-18 11:14:25 -0400

    Add Welcome Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Welcome to My Cloudera Blog!",
+        "excerpt":"This is my first post for my new GitHub Pages deployed website. This was something I did in my previous role @Datastax and you can find that page here. Please give me some time to get familar with the Minimal Mistakes theme and the changes in capability and functionality with...","categories": ["blog"],
+        "tags": ["update"],
+        "url": "/blog/welcome/",
+        "teaser": "/assets/images/500x300.png"
+      }]
