4b8b810a761064676f37921deefd665ad6e470e7
Steven Matison <98845751+cldr-steven-matison@users.noreply.github.com>
2023-05-18 11:26:31 -0400

    Add Dataflow Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Newest NiFi @Cloudera",
+        "excerpt":"I wrote my first @Cloudera Community article on how to Operationalize NiFi with CDP Public Cloud’s DataFlow Data Service. NiFi is something I have been excited about for a very long time. A major focus of this blog and my social media content will be around all of the new...","categories": ["blog"],
+        "tags": ["nifi"],
+        "url": "/blog/dataflow/",
+        "teaser": "/assets/images/500x300.png"
+      },{
         "title": "Welcome to My Cloudera Blog!",
         "excerpt":"This is my first post for my new GitHub Pages deployed website. This was something I did in my previous role @Datastax and you can find that page here. Please give me some time to get familar with the Minimal Mistakes theme and the changes in capability and functionality with...","categories": ["blog"],
         "tags": ["update"],
