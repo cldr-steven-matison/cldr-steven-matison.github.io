b7590e3fabf6deb28bc5b5bcb132acc196324ba0
Steven Matison <36509640+steven-matison@users.noreply.github.com>
2023-06-06 08:57:02 -0400

    Update Welcome Image

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,7 +3,7 @@ var store = [{
         "excerpt":"This is my first post for my new GitHub Pages deployed website. This was something I did in my previous role @Datastax and you can find that page here. Please give me some time to get familar with the Minimal Mistakes theme and the changes in capability and functionality with...","categories": ["blog"],
         "tags": ["update"],
         "url": "/blog/welcome/",
-        "teaser": "/assets/images/srm.png"
+        "teaser": "/assets/images/SRM.png"
       },{
         "title": "Newest NiFi @Cloudera",
         "excerpt":"I wrote my first @Cloudera Community article on how to Operationalize NiFi with CDP Public Cloud’s DataFlow Data Service. NiFi is something I have been excited about for a very long time. A major focus of this blog and my social media content will be around all of the new...","categories": ["blog"],
