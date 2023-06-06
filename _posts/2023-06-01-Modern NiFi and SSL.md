974864e8c4eb36d5cca37d5e651d599d935ec4ce
Steven Matison <36509640+steven-matison@users.noreply.github.com>
2023-06-06 08:43:00 -0400

    New Post and Some Updates

---
+++ _posts/2023-06-01-Modern NiFi and SSL.md
@@ -0,0 +1,15 @@
+---
+title:  "NIFI SSL in Modern Versions of NiFi"
+header:
+  teaser: "/assets/images/2023-06-01-Modern NiFi and SSL.png"
+categories: 
+  - blog
+tags:
+  - nifi
+---
+
+My original experiences with NiFi and SSL were not something I would call fun or easy.  Literally hours and days were spent configuring nifi cluster nodes to communicate with each other using SSL and enabling https Nifi UI.  Adding access and authentication controls was even more hard work. Last but not least, configuring SSL Context Services to communicate with external https endpoints was always a challenge.   I am happy to report that it is very easy to install modern versionf of NiFi.  Out of the box these versions comes fully secured with ssl and an initial user and password for login.     No more fully exposed IP addresses w/ nifi canvas ready for abuse.  Join me as I explore a new nifi install and create 2 sample SSL Context Services.  
+
+You can read the full article over at the Cloudera Community:
+
+[NIFI SSL in Modern Versions of NiFi](https://community.cloudera.com/t5/Community-Articles/NIFI-SSL-in-Modern-Versions-of-NiFi/ta-p/371937)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,13 +3,13 @@ var store = [{
         "excerpt":"This is my first post for my new GitHub Pages deployed website. This was something I did in my previous role @Datastax and you can find that page here. Please give me some time to get familar with the Minimal Mistakes theme and the changes in capability and functionality with...","categories": ["blog"],
         "tags": ["update"],
         "url": "/blog/welcome/",
-        "teaser": "/assets/images/500x300.png"
+        "teaser": "/assets/images/srm.png"
       },{
         "title": "Newest NiFi @Cloudera",
         "excerpt":"I wrote my first @Cloudera Community article on how to Operationalize NiFi with CDP Public Cloud’s DataFlow Data Service. NiFi is something I have been excited about for a very long time. A major focus of this blog and my social media content will be around all of the new...","categories": ["blog"],
         "tags": ["nifi"],
         "url": "/blog/dataflow/",
-        "teaser": "/assets/images/500x300.png"
+        "teaser": "/assets/images/2023-05-18-dataflow.png"
       },{
         "title": "Converting NiFi XML to JSON with NiFi",
         "excerpt":"I wanted to work with some old nifi xml templates in DataFlow which requires new nifi json flow definition files. To get these templates converted I had to import them to an operational nifi, drag them onto the screen, and download the flow definition file I needed. This seemed like...","categories": ["blog"],
@@ -22,4 +22,10 @@ var store = [{
         "tags": ["nifi"],
         "url": "/blog/NIFI-11608/",
         "teaser": "/assets/images/2023-05-31-NIFI%2011608-1.png"
+      },{
+        "title": "NIFI SSL in Modern Versions of NiFi",
+        "excerpt":"My original experiences with NiFi and SSL were not something I would call fun or easy. Literally hours and days were spent configuring nifi cluster nodes to communicate with each other using SSL and enabling https Nifi UI. Adding access and authentication controls was even more hard work. Last but...","categories": ["blog"],
+        "tags": ["nifi"],
+        "url": "/blog/Modern-NiFi-and-SSL/",
+        "teaser": "/assets/images/2023-06-01-Modern%20NiFi%20and%20SSL.png"
       }]
