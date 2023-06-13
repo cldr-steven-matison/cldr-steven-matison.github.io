fa19ce358ad951c0682845528a31f73d22ccab94
Steven Matison <36509640+steven-matison@users.noreply.github.com>
2023-06-13 13:33:07 -0400

    Update Test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Welcome to My Cloudera Blog!",
-        "excerpt":"This is my first post for my new GitHub Pages deployed website. This was something I did in my previous role @Datastax and you can find that page here. Please give me some time to get familar with the Minimal Mistakes theme and the changes in capability and functionality with...","categories": ["blog"],
+        "title": "Welcome to My Cloudera Blog UPDATED!",
+        "excerpt":"UPDATED This is my first post for my new GitHub Pages deployed website. This was something I did in my previous role @Datastax and you can find that page here. Please give me some time to get familar with the Minimal Mistakes theme and the changes in capability and functionality...","categories": ["blog"],
         "tags": ["update"],
         "url": "/blog/welcome/",
         "teaser": "/assets/images/SRM.png"
